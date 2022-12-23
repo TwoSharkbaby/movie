13686269eeb8ef787eb8f537660ad7cac7e4fe4c
TwoSharkbaby <wjdekdns6602@naver.com>
2022-12-23 16:46:54 +0900

    movie / review / member / actor crud

--- movie/src/main/java/org/zerock/mapper/MovieReviewMapper.java
+++ movie/src/main/java/org/zerock/mapper/MovieReviewMapper.java
@@ -1,5 +1,17 @@
 package org.zerock.mapper;
 
+import java.util.List;
+
+import org.zerock.domain.MovieReviewVO;
+
 public interface MovieReviewMapper {
 
+	public List<MovieReviewVO> getList();
+	
+	public MovieReviewVO read(Long mov_rev_num);
+
+	public void insert(MovieReviewVO movieReviewVO);
+
+	public int delete(Long mov_rev_num);
+	
 }
---
+++ movie/src/test/java/org/zerock/persistence/MovieReviewMapperTests2.java
@@ -0,0 +1,77 @@
+package org.zerock.persistence;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.zerock.domain.MovieReviewVO;
+import org.zerock.mapper.MovieReviewMapper;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Log4j
+
+public class MovieReviewMapperTests2 {
+	
+	@Setter(onMethod_ = @Autowired)
+	private MovieReviewMapper movieReviewMapper;
+
+	@Test
+	public void testGetList() {
+		log.info("----------------------------------------------");
+		movieReviewMapper.getList().forEach(b -> log.info(b));
+		log.info("----------------------------------------------");
+
+	}
+
+	@Test
+	public void testRead() {
+		log.info("----------------------------------------------");
+		movieReviewMapper.read(1L);
+		log.info("----------------------------------------------");
+	}
+
+	@Test
+	public void testInsert() {
+		log.info("----------------------------------------------");
+		MovieReviewVO movieReviewVO = MovieReviewVO.builder().mov_rev_title("123").mov_num(1L).mem_num(1L).build();
+		movieReviewMapper.insert(movieReviewVO);
+		log.info("----------------------------------------------");
+	}
+
+	@Test
+	public void testDelete() {
+		log.info("----------------------------------------------");
+		movieReviewMapper.delete(6L);
+		log.info("----------------------------------------------");
+	}
+	
+//	@Test
+//	public void testTotal() {
+//		Criteria cri = new Criteria();
+//		cri.setType("TC");
+//		cri.setKeyword("?Öå?ä§?ä∏");
+//		log.info(".........................Total:" + boardMapper.getTotalCount(cri));
+//	}
+//	
+//	@Test
+//	public void testGetListWithPaging() {
+//		Criteria cri = new Criteria();
+//		cri.setType("TC");
+//		cri.setKeyword("?Öå?ä§?ä∏");
+//		boardMapper.getListWithPaging(cri);
+//	}
+//	@Test
+//	public void testUpdate() {
+//		BoardVO vo = BoardVO.builder().bno(3L).title("?Çú?Çò?Çú").content("Î¨¥ÎÇò?Çú").writer("Î£®Îùº?ûÑ").build();
+//		int result = boardMapper.update(vo);
+//		log.info("===========================================================");
+//		log.info("count.............." + result);
+//		log.info("===========================================================");
+//	}
+
+}
