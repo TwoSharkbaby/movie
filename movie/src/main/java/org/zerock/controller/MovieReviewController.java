f99065c0a8f391026f63fbc6967a5c394a0f5a20
TwoSharkbaby <wjdekdns6602@naver.com>
2022-12-28 10:21:24 +0900

    #

--- movie/src/main/java/org/zerock/controller/MovieReviewController.java
+++ movie/src/main/java/org/zerock/controller/MovieReviewController.java
@@ -1,12 +1,10 @@
 package org.zerock.controller;
 
-import java.util.List;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.zerock.domain.MovieReviewVO;
@@ -16,7 +14,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j;
 
 @RestController // REST + Controller
-@RequestMapping("/replies/*")
+@RequestMapping("/review/*")
 @RequiredArgsConstructor
 @Log4j
 public class MovieReviewController {
@@ -28,6 +26,15 @@ public class MovieReviewController {
 //	public ResponseEntity<List<MovieReviewVO>> getList(@PathVariable Long mov_num) {
 //		return new ResponseEntity<>(movieReviewService.movieReviewRead(mov_num), HttpStatus.OK);
 //	}
+	
+	@PostMapping(value = "/good", consumes = "application/json")
+	public ResponseEntity<String> goodUpdate(@RequestBody MovieReviewVO movieReviewVO) {
+		log.info("movieReviewVO" + movieReviewVO);
+
+//		int insertCount = movieReviewService.goodUpdate()
+
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
 
 
 
